5cb9270aaac51a0797684781f2d94870ff95f1c3
alesandi11 <146051226+alesandi11@users.noreply.github.com>
2024-06-10 14:00:46 +0700

    Add files via upload

---
+++ StukturData/meet9.py
@@ -0,0 +1,23 @@
+#percabangan jika ada 
+
+name = input ("what is your name : ")
+
+#if kondisi : aksi
+
+# 1. if standar 
+if name == "sandi" : print("mahasiswa")
+print("and of program")
+
+# 2. if indentasi
+if name== "sandi" :
+    print("mahasiswa")
+    print("mahasiswa")
+print("and of program")
+
+# 3. if else 
+if name=="sandi" :
+    print("sandi ")
+    print(f"terimakasih{name}")
+else:
+    print("anda tidak mahasiswa")
+print("and of program")
